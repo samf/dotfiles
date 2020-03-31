49fff92956598cce63566bd6aa2f3b38f4e146e6
Sam Falkner <sam.falkner@ibm.com>
2020-03-31 15:30:54 -0600

    vundle help

--- .vim/vundle.sh
+++ .vim/vundle.sh
@@ -1,3 +1,13 @@
 #! /bin/sh -xe
 
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+test -d Vundle.vim || {
+    cat <<EOF
+    You need to install python3-venv either by
+    sudo apt install python3-venv
+    or
+    brew install python3 && pip3 install virtualenv
+    You must do this before you run VundleUpdate!
+EOF
+    git clone https://github.com/VundleVim/Vundle.vim.git \
+        $HOME/.vim/bundle/Vundle.vim
+}
